2e40b0bec43d80fa51fa1169c0f235d2e864d1a6
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2025-07-24 16:40:50 -0500

    feat: add token validation and redirect to login in dashboard component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  constructor(private router: Router) {}
+  
+  ngOnInit() {
+    if (!localStorage.getItem('token')) {
+      this.router.navigate(['/login']); // Redirigir al dashboard si ya está autenticado
+    }
+  }
 
   logOut() {
     localStorage.removeItem('token');
